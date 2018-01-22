a881d63de18d97f66cbd5afb68d6ecab45b4ac13
Steve Thompson <writetome51@gmail.com>
2018-01-22 12:28:07 -0700

    new commit

--- src/SavingsAccount.ts
+++ src/SavingsAccount.ts
@@ -1,13 +1,54 @@
 import { AbstractAccount } from './AbstractAccount';
 import {Transaction} from "./Transaction.interface";
+import {TransactionOrigin} from "./TransactionOrigin";
 
 
 
 export class SavingsAccount extends AbstractAccount {
 
+	balance = 10000;
+
 	constructor(){
 		super();
 
-		this.balance = 10000;
 	}
+
+
+	withdrawMoney(amount: number, description: string, transactionOrigin: TransactionOrigin): Transaction{
+		if ((transactionOrigin === TransactionOrigin.phone || transactionOrigin === TransactionOrigin.web) &&
+			this._hasSixWithdrawalsFromWebAndPhone()){
+			let thisTransaction = {
+				success: false,
+				amount: -(amount),
+				resultBalance: this.balance,
+				transactionDate: new Date(),
+				description: description,
+				transactionOrigin:transactionOrigin,
+				errorMessage: 'You have met your limit of 6 withdrawals by phone or web this month.'
+			};
+			return thisTransaction;
+		}
+		else{
+			return super.withdrawMoney(amount, description, transactionOrigin);
+		}
+
+	}
+
+
+	private _hasSixWithdrawalsFromWebAndPhone(){
+		let numPhoneOrWebTransactions = 0;
+
+		this.accountHistory.forEach(
+			function(transaction){
+				if (transaction.success && transaction.transactionOrigin &&
+					(transaction.transactionOrigin === TransactionOrigin.web ||
+						transaction.transactionOrigin === TransactionOrigin.phone)){
+					++numPhoneOrWebTransactions;
+				}
+			}
+		);
+		return (numPhoneOrWebTransactions >= 6);
+	}
+
+
 }
