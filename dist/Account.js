ff7eccfec10bae66d5c80a4b98c0eddfcddb359f
Steve Thompson <writetome51@gmail.com>
2018-01-22 18:02:01 -0700

    absolute final commit

--- dist/Account.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var getAccount;
-getAccount = function (id, name) {
-    var b = '';
-    return [id, name, b];
-};
-getAccount('', '');
-//# sourceMappingURL=Account.js.map
